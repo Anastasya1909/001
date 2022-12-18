9867011d9fae17836baff7ccb09b165a9aa0fff3
Anastasya <ocenka.da@gmail.com>
2022-12-18 20:36:42 +0700

    Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее

---
+++ Дз1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = -9;
+int b = -3;
+
+
+int max = a;
+int min = a;
+
+if (a > max) 
+{
+    max = a; 
+    min = b;
+}
+if (b > max) 
+{
+    max = b; 
+    min = a;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
